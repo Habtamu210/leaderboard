799831f9697c83b9e9d3233e06b2e47ff2e07cb3
Habtamu210 <beleyaha@gmail.com>
2023-02-07 02:19:42 -0800

    Update change

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { result } from './module/result.js';
+import './style.css';
+
+function addScore(score) {
+  const list = document.querySelector('.score-list');
+  const li = document.createElement('li');
+  li.innerHTML = `${score.name}: ${score.score}`;
+  list.appendChild(li);
+}
+
+result.forEach((i) => {
+  addScore(i);
+});
---
+++ src/module/result.js
@@ -0,0 +1,32 @@
+const result = [
+  {
+    name: 'Tize',
+    score: 100,
+  },
+  {
+    name: 'Hana',
+    score: 20,
+  },
+  {
+    name: 'Alemu',
+    score: 50,
+  },
+  {
+    name: 'Bina',
+    score: 78,
+  },
+  {
+    name: 'Maya',
+    score: 125,
+  },
+  {
+    name: 'Sina',
+    score: 77,
+  },
+  {
+    name: 'Bohar',
+    score: 42,
+  },
+];
+// eslint-disable-next-line import/prefer-default-export
+export { result };
