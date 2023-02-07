31fe04bc93f0f2875193b808f008f873aa98182f
Habtamu210 <beleyaha@gmail.com>
2023-02-07 03:50:00 -0800

    Update file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { result } from './module/result.js';
 import './style.css';
 
 function addScore(score) {
-  const list = document.querySelector('.score-list');
+  const list = document.querySelector('.score-result');
   const li = document.createElement('li');
   li.innerHTML = `${score.name}: ${score.score}`;
   list.appendChild(li);
